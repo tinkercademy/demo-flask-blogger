e34d9979840e55949ec2cd65e1d2c3cf8f103f8c
YJ Soon <yjsoon@tinkertanker.com>
2018-12-18 03:13:39 +0800

    Basic database creation

--- server.py
+++ server.py
@@ -1,11 +1,33 @@
 import flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, flash, redirect, url_for
+import os
+import sqlite3
 app = flask.Flask(__name__)
+
+def get_db():
+    db = sqlite3.connect('db.sqlite3')
+    db.row_factory = sqlite3.Row
+    return db
+
+def create_db():
+    db = get_db()
+    db.execute('CREATE TABLE post ' + \
+               '(id INTEGER PRIMARY KEY AUTOINCREMENT, ' + \
+               'title TEXT NOT NULL, ' + \
+               'body TEXT, ' + \
+               'image TEXT, ' + \
+               'file TEXT)')
+    db.close()
+
+if not os.path.isfile('db.sqlite3'):
+    create_db()
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    db = get_db()
+    posts = get_db().execute('SELECT * FROM post').fetchall()
+    db.close()
+    return render_template("index.html", posts=posts)
 
 @app.errorhandler(404)
 def page_not_found(e):
